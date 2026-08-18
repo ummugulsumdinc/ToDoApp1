8d09c7bca575fecff564cf073195c2e35f56c88b
Ümmügülsüm Dinç <dincummugulsum11@gmail.com>
2026-08-18 09:47:00 +0300

    todoapp yazıldı

---
+++ ToDoApp1/Business/Dtos/ToDoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoApp1.Business.Dtos
+{
+    public class ToDoDto
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ ToDoApp1/Business/Interfaces/IToDoService.cs
@@ -0,0 +1,18 @@
+﻿using ToDoApp1.Business.Dtos;
+
+namespace ToDoApp1.Business.Interfaces
+{
+    public interface IToDoService
+    {
+        
+        List<ToDoDto> GetAll();
+
+        ToDoDto? GetById(int id);
+
+        void PutAdd(ToDoDto todo);
+
+        void Update(ToDoDto todo);
+
+        void Delete(int id);
+    }
+}
